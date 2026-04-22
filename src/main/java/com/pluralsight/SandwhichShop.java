8b8457856c0e8ee41c41fb51a3c2ac93f31abe56
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-04-22 02:45:09 -0500

    inserted if else for discount and create new sandwhich cost methods variables for math

--- src/main/java/com/pluralsight/SandwhichShop.java
+++ src/main/java/com/pluralsight/SandwhichShop.java
@@ -4,11 +4,10 @@ import java.util.Scanner;
 
 public class SandwhichShop {
     // create variables for the prices of the sandwhiches and the discounts
-    static double regular;
-    static double large;
-    static double youngin;
-    static double mature;
-    static double noDiscount;
+    static double regularPrice = 5.45;
+    static double largePrice = 8.95;
+    static double loadedExtraRegular = 1.00;
+    static double loadedExtraLarge = 1.75;
 
     public static void main(String[] args) {
         //insert scanner
@@ -16,88 +15,84 @@ public class SandwhichShop {
 
         //ask user for the size of sandwiches using 1/2
         System.out.println("What size sandwhich do you want to do, Regular(a) or Large(b)?");
-        String command = scanner.nextLine();
+        String command = scanner.nextLine().toLowerCase();
+
+        double basePrice = 0;
+        boolean isLarge = false;
 
         // insert if statement to figure out the size of the sandwhich and print out the price
         if (command.equals("regular") || command.equals("a")) {
-            regular = 5.45;
-            System.out.println("1: Regular: Base price is $5.45");
+            basePrice = regularPrice;
+            System.out.println("1: Regular: Base price is $" + String.format("%.2f", basePrice));
         } else if (command.equals("large") || command.equals("b")) {
-            large = 8.95;
-            System.out.println("2: Large: Base price is $8.95");
+            basePrice = largePrice;
+            isLarge = true;
+            System.out.println("2: Large: Base price is $" + String.format("%.2f", basePrice));
         } else {
-            noDiscount = 0.00;
             System.out.println("Invalid, please try again!");
+            return;
         }
 
         // Call ageChecker method
-        ageChecker(scanner);
+        double discount = ageChecker(scanner);
+
+        // Call loadedSandwhich method
+        double extra = loadedSandwhich(scanner, isLarge);
+
+        basePrice += extra;
 
         // Get sandwhichCost method
-        sandwhichCost(scanner, regular, youngin, mature, noDiscount);
+        sandwhichCost(basePrice, discount);
     }
 
-    public static void ageChecker(Scanner scanner) {
+    public static double ageChecker(Scanner scanner) {
 
         // prompt user for their age in a new method
         System.out.println("How old are you?");
-        Double age = scanner.nextDouble();
+        String ageStr = scanner.nextLine();
+        double age = Double.parseDouble(ageStr);
 
         // create if statement to see what discount the user will receive
         if (age <= 17) {
-            youngin = 0.10;
             System.out.println("You get a 10% discount");
+            return 0.10;
         } else if (age >= 65) {
-            mature = 0.15;
             System.out.println("You get a 15% discount");
+            return 0.15;
         } else {
-            noDiscount = 0.00;
             System.out.println("Sorry, you do not get a discount");
+            return 0.00;
         }
     }
+    public static double loadedSandwhich(Scanner scanner, boolean isLarge) {
+        // ask if user wants their sandwhich loaded
+        System.out.println("Do you want your sandwhich loaded? (yes/no)");
+        String loadedCommand = scanner.nextLine().toLowerCase();
 
-    public static void sandwhichCost(Scanner scanner, double regular, double youngin, double mature, double noDiscount) {
-       
-        // base price for regular and under 17 cost
-        double regularYoungin = regular - (regular * youngin);
+        double extra = 0;
 
-        // base price for regular and over 65 cost
-        double regularMature = regular - (regular * mature);
-
-        // base price for regular and no discount cost
-        double regularNoDiscount = regular - (regular * noDiscount);
-
-        // base price for large and under 17 cost
-        double largeYoungin = large - (large * youngin);
-
-        // base price for large and over 65 cost
-        double largeMature = large - (large * mature);
-
-        // base price for large and no discount cost
-        double largeNoDiscount = large - (large * noDiscount);
-
-        // Determine which size and discount apply then print the final price
-        double finalPrice = 0;
-        
-        if (regular > 0) {
-            // Regular size was selected
-            if (youngin > 0) {
-                finalPrice = regularYoungin;
-            } else if (mature > 0) {
-                finalPrice = regularMature;
+        // create a method for the loaded sandwhich and print out the price
+        if (loadedCommand.equals("yes")) {
+            if (isLarge) {
+                extra = loadedExtraLarge;
             } else {
-                finalPrice = regularNoDiscount;
+                extra = loadedExtraRegular;
             }
-        } else if (large > 0) {
-            // Large size was selected
-            if (youngin > 0) {
-                finalPrice = largeYoungin;
-            } else if (mature > 0) {
-                finalPrice = largeMature;
-            } else {
-                finalPrice = largeNoDiscount;
-            }
+            double loadedPrice = (isLarge ? largePrice : regularPrice) + extra;
+            System.out.println("Loaded " + (isLarge ? "Large" : "Regular") + ": Base price is $" + String.format("%.2f", loadedPrice));
+        } else if (loadedCommand.equals("no")) {
+            // insert else statement for user not wanting loaded sandwhich
+            System.out.println("You chose not to load your sandwhich");
+        } else {
+            // insert else statement for invalid input
+            System.out.println("Invalid, please try again!");
         }
+        return extra;
+    }
+
+    public static void sandwhichCost(double basePrice, double discount) {
+       // print out the final price of the sandwhich after applying the discount
+        double finalPrice = basePrice * (1 - discount);
 
         System.out.println("Your final price is: $" + String.format("%.2f", finalPrice));
     }
