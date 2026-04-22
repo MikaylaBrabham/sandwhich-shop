d96728b29856f79d93ad02baf8bb26d0ebe00377
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-04-22 01:03:08 -0500

    added sizes and their if statements

--- src/main/java/com/pluralsight/SandwhichShop.java
+++ src/main/java/com/pluralsight/SandwhichShop.java
@@ -8,6 +8,15 @@ public class SandwhichShop {
         Scanner scanner = new Scanner(System.in);
 
         //ask user for the size of sandwiches using 1/2
+        System.out.println("What size sandwhich do you want to do, Regular(a) or Large(b)?");
+        String command = scanner.nextLine();
+
+        // insert if statement to figure out the size of the sandwhich and print out the price
+        if (command.equals("regular") || command.equals("a")) {
+            System.out.println("1: Regular: Base price is $5.45");}
+        else if (command.equals("large") || command.equals("b")) {
+            System.out.println("2: Large: Base price is $8.95");
+        }
 
 
     }
