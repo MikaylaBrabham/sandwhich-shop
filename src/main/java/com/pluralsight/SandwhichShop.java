4d4b772d4e96623c0ffc822f89b920832155e51b
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-04-22 00:39:40 -0500

    added scanner and added comment asking about the sizes

---
+++ src/main/java/com/pluralsight/SandwhichShop.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class SandwhichShop {
+    public static void main(String[] args) {
+        //insert scanner
+        Scanner scanner = new Scanner(System.in);
+
+        //ask user for the size of sandwiches using 1/2
+
+
+    }
+}
